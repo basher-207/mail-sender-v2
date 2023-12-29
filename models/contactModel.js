1301553ed9a69085d2994ddeec98061318a90ed6
Vladyslav Diachenko <basher207vd@gmail.com>
2023-12-29 16:51:18 +0200

    Add contactModel in ./models/contactModel.js

---
+++ models/contactModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Contact name must be specified'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, "Contact email must be specified"],
+    unique: [true, "Contact with this email is already exist"]
+  },
+  // letters: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'Letter',
+  // }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
